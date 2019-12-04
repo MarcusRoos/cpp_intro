defad5d4b5b0b834b8ab005e5d8aa7fd82f9a38c
Marcus Roos <Marcusrs_roos@outlook.com>
2019-12-04 18:36:34 +0100

    Laboration 2 - Marcus Roos Senast uppdaterad: 4/12-2019
    Får det mesta att funka, får däremot inte in nollor vid varje versal, känns som att jag har försökt med allt.
    Fortsätter att pilla runt med det tills det fungerar,
    jag kan ersätta versaler med nollor samt lägga till nollor vid versaler,
    men så fort jag blandar in ROT 7/9 slutar det funka.
    
    Löste problemet, jag började istället med att lägga dit nollorna, sedan ignorera dem när jag gör rotationerna.
    I slutet var jag tvungen att ta bort det sista tecknet (blev ett white space), hittills har jag ingen finare lösning.
    Jag fortsätter arbeta på det fram tills deadline.

--- Laboration_2/src/Definitions.cpp
+++ Laboration_2/src/Definitions.cpp
@@ -19,15 +19,13 @@ bool authenticateUser(string value) {
     const string USERNAME = "Kalle";
     const string PASSWORD = "i0J0u0j0u0J0Zys0r0{";
     bool authPassed = false;
-    string password, username, pass;
+    string username, password;
 
     password = value.substr(value.find(',') + 1);
     username = value.erase(value.find(',')), password.length();
-    pass = decryptPassword(password);
+    string pass = decryptPassword(password);
 
     authPassed = pass == PASSWORD && username == USERNAME;
-    cout << pass << endl;
-
 
     return authPassed;
 }
@@ -35,34 +33,51 @@ bool authenticateUser(string value) {
 string decryptPassword(string pass) {
     const int ROT7 = 7, ROT9 = 9;
     const string VOWELS = "AEIOUYaeiouy";
-    string decrypted, password;
+    string decrypted, password, decrypt;
+    password = pass;
+    decrypted = pass;
+
+    int encryptIndex = 0;
+    for (int i = 0; i < password.length(); i++, encryptIndex++){
+        for (int j=0; j<VOWELS.length(); j++){
+            if (password[i] == VOWELS[j]){
+                decrypted.insert(encryptIndex, 1, '0');
+                decrypted.insert(encryptIndex + 2, +1, '0');
+                encryptIndex += 2;
+                break;
+            }
+        }
+    }
+
     int j=0;
     for (int i=0; i<pass.length(); i++){
-
-
-        if((pass[j]+ROT7) %2==0) {
-            decrypted += pass[j] + ROT7;
+        if((decrypted[j]+ROT7) %2==0) {
+            decrypt += decrypted[j] + ROT7;
             j++;
             goto here;
         }
         else{
-        for (int k=0; k < 1; k++) {
-                decrypted += int(pass[j] + ROT7);
+            if (decrypted[j] == '0'){
+                decrypt += int(decrypted[j]);
                 j++;
             }
-        here:
-        for (int p=0; p< 1; p++) {
-            decrypted += int(pass[j] + ROT9);
-            j++;
-        }
+            for (int k=0; k < 1; k++) {
+                decrypt += int(decrypted[j] + ROT7);
+                j++;
+            }
+            if (decrypted[j] == '0'){
+                decrypt += int(decrypted[j]);
+                j++;
+            }
+            here:
+            for (int k=0; k < 1; k++) {
+                decrypt += int(decrypted[j] + ROT9);
+                j++;
+            }
             i++;
         }
     }
-    j = 0;
-    string decryptedTemp = decrypted;
-
-
-
-    return decrypted;
+    decrypt.erase(decrypt.size()-1); //Tar bort en whitespace.
+    return decrypt;
 
 }
