9b68fe43a7c72cc018218f321b79f50179a2baa2
Marcus Roos <Marcusrs_roos@outlook.com>
2019-12-06 03:24:38 +0100

    Laboration 2 - Marcus Roos Senast uppdaterad: 6/12-2019
    /*Får det mesta att funka, får däremot inte in nollor vid varje versal, känns som att jag har försökt med allt.
    Fortsätter att pilla runt med det tills det fungerar,
    jag kan ersätta versaler med nollor samt lägga till nollor vid versaler,
    men så fort jag blandar in ROT 7/9 slutar det funka.*/
    
    Löste problemet, jag började istället med att lägga dit nollorna, sedan ignorera dem när jag gör rotationerna.
    I slutet var jag tvungen att ta bort det sista tecknet (blev ett white space), hittills har jag ingen finare lösning.
    Jag fortsätter arbeta på det fram tills deadline.

--- Laboration_2/src/Definitions.cpp
+++ Laboration_2/src/Definitions.cpp
@@ -4,10 +4,9 @@ using std::stringstream;
 using std::cout;
 using std::endl;
 
-
 using namespace std;
 
-string mainArgumentParser(int argc, char* argv[]) {
+string mainArgumentParser(int argc, char * argv[]) {
     if (argc < 3)
         return "fail";
     stringstream ss;
@@ -21,63 +20,66 @@ bool authenticateUser(string value) {
     bool authPassed = false;
     string username, password;
 
-    password = value.substr(value.find(',') + 1);
-    username = value.erase(value.find(',')), password.length();
-    string pass = decryptPassword(password);
+    password = value.substr(value.find(',') + 1); //särar på lösenordet och användarnamnet, lagrar i substrings.
+    username = value.erase(value.find(',')), password.length(); //särar på lösenordet och användarnamnet, lagrar i substring.
+    string pass = decryptPassword(password); //kallar funktionen decryptPassword med lagrade värdet av password.
 
-    authPassed = pass == PASSWORD && username == USERNAME;
-
+    if (pass == PASSWORD && username == USERNAME) { //om decryptPassword funktionens returvärde stämmer överens, ge autoPassed sant.
+        authPassed = true;
+    }
+    cout << endl << pass << endl;
     return authPassed;
 }
 
 string decryptPassword(string pass) {
-    const int ROT7 = 7, ROT9 = 9;
+    const int ROT7 = 7,
+            ROT9 = 9;
     const string VOWELS = "AEIOUYaeiouy";
     string decrypt, password, decrypted;
     password = pass;
     decrypt = pass;
 
     int encryptIndex = 0;
-    for (int i = 0; i < password.length(); i++, encryptIndex++){
-        for (char j : VOWELS){
-            if (password[i] == j){
+    for (int i = 0; i < password.length(); i++, encryptIndex++) { //använder index för att gå igenom hela lösenordets längd.
+        for (char j: VOWELS) {
+            /*använder nested for-loop för att gå igenom varje karaktär
+             *                              lagrad i VOWELS, söker igenom varje karaktär i lösenordet.*/
+            if (password[i] == j) {
                 decrypt.insert(encryptIndex, 1, '0');
                 decrypt.insert(encryptIndex + 2, +1, '0');
-                encryptIndex += 2;
+                encryptIndex += 2; //Lägger till två i indexet då 2 nollor har lagts till, annars går den inte igenom hela lösenordet.
                 break;
             }
         }
     }
-
-    int j=0;
-    for (int i=0; i<pass.length(); i++){
-        if((decrypt[j]+ROT7) %2==0) {
+    int j = 0;
+    for (int i = 0; i < pass.length(); i++) { //Iteration så länge i är mindre är lösenordets längd.
+        int k = 0, l = 0;
+        if ((decrypt[j] + ROT7) % 2 == 0) { //Om nuvarande karaktär i lösenordet +7 rotation är jämt, använd ROT7.
             decrypted += decrypt[j] + ROT7;
             j++;
-            goto here;
-        }
-        else{
-            if (decrypt[j] == '0'){
-                decrypted += int(decrypt[j]);
+        } else {
+                if (decrypt[j] == '0') { //Om en nolla uppstår, hoppa över genom att öka j med ett.
+                    decrypted += (decrypt[j]);
+                    j++;
+                }
+            while (k < 1) { //Använd ROT7 en gång, fortsätt sedan till ROT9.
+                decrypted += (decrypt[j] + ROT7);
                 j++;
+                k++;
             }
-            for (int k=0; k < 1; k++) {
-                decrypted += int(decrypt[j] + ROT7);
+                if (decrypt[j] == '0') { //Om en nolla uppstår, hoppa över genom att öka j med ett.
+                    decrypted += (decrypt[j]);
+                    j++;
+                }
+            while (l < 1) {
+                decrypted += (decrypt[j] + ROT9); //Använd ROT9 en gång, fortsätt sedan till ROT7.
                 j++;
-            }
-            if (decrypt[j] == '0'){
-                decrypted += int(decrypt[j]);
-                j++;
-            }
-            here:
-            for (int k=0; k < 1; k++) {
-                decrypted += int(decrypt[j] + ROT9);
-                j++;
+                l++;
             }
             i++;
         }
     }
-    decrypted.erase(decrypted.size()-1); //Tar bort en whitespace, hittar ingen snyggare lösning... än.
+    decrypted.erase(decrypted.size() - 1); //Tar bort en whitespace, hittar ingen snyggare lösning... än.
     return decrypted;
-
 }
