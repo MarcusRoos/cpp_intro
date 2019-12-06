e81a64cec98d2e6b91449ef536721a0992b6381c
Marcus Roos <Marcusrs_roos@outlook.com>
2019-12-06 03:55:59 +0100

    Laboration 2 - Marcus Roos Senast uppdaterad: 6/12-2019
    Snyggar till texten.

--- Laboration_2/src/Definitions.cpp
+++ Laboration_2/src/Definitions.cpp
@@ -20,11 +20,15 @@ bool authenticateUser(string value) {
     bool authPassed = false;
     string username, password;
 
-    password = value.substr(value.find(',') + 1); // särar på lösenordet och användarnamnet, lagrar i substrings.
-    username = value.erase(value.find(',')), password.length(); // särar på lösenordet och användarnamnet, lagrar i substring.
-    string pass = decryptPassword(password); // kallar funktionen decryptPassword med lagrade värdet av password.
+    // särar på lösenordet och användarnamnet, lagrar i substrings.
+    password = value.substr(value.find(',') + 1);
+    // särar på lösenordet och användarnamnet, lagrar i substring.
+    username = value.erase(value.find(',')), password.length();
+    // kallar funktionen decryptPassword med lagrade värdet av password.
+    string pass = decryptPassword(password);
 
-    if (pass == PASSWORD && username == USERNAME) { // om decryptPassword funktionens returvärde stämmer överens, ge autoPassed sant.
+    // om funktionens returvärde stämmer överens, ge autoPassed sant.
+    if (pass == PASSWORD && username == USERNAME) {
         authPassed = true;
     }
     cout << endl << pass << endl;
@@ -40,46 +44,56 @@ string decryptPassword(string pass) {
     decrypt = pass;
 
     int encryptIndex = 0;
-    for (int i = 0; i < password.length(); i++, encryptIndex++) { // använder index för att gå igenom hela lösenordets längd.
+    // använder index för att gå igenom hela lösenordets längd.
+    for (int i = 0; i < password.length(); i++, encryptIndex++) {
+ /* använder nested for-loop för att gå igenom varje karaktär
+ * lagrad i VOWELS, söker igenom varje karaktär i lösenordet.*/
         for (char j: VOWELS) {
-            /* använder nested for-loop för att gå igenom varje karaktär
-             * lagrad i VOWELS, söker igenom varje karaktär i lösenordet.*/
             if (password[i] == j) {
                 decrypt.insert(encryptIndex, 1, '0');
                 decrypt.insert(encryptIndex + 2, +1, '0');
-                encryptIndex += 2; // Lägger till två i indexet då 2 nollor har lagts till, annars går den inte igenom hela lösenordet.
+                /* Lägger till två i indexet då 2 nollor har lagts till, annars
+                 * går den inte igenom hela lösenordet. */
+                encryptIndex += 2;
                 break;
             }
         }
     }
     int j = 0;
-    for (int i = 0; i < pass.length(); i++) { // Iteration så länge i är mindre är lösenordets längd.
+    // Iteration så länge i är mindre är lösenordets längd.
+    for (int i = 0; i < pass.length(); i++) {
         int k = 0, l = 0;
-        if ((decrypt[j] + ROT7) % 2 == 0) { // Om nuvarande karaktär i lösenordet +7 rotation är jämt, använd ROT7.
+        // Om nuvarande karaktär i lösenordet +7 rotation är jämt, använd ROT7.
+        if ((decrypt[j] + ROT7) % 2 == 0) {
             decrypted += decrypt[j] + ROT7;
             j++;
         } else {
-                if (decrypt[j] == '0') { // Om en nolla uppstår, hoppa över genom att öka j med ett.
+            // Om en nolla uppstår, hoppa över genom att öka j med ett.
+                if (decrypt[j] == '0') {
                     decrypted += (decrypt[j]);
                     j++;
                 }
-            while (k < 1) { // Använd ROT7 en gång, fortsätt sedan till ROT9.
+            // Använd ROT7 en gång, fortsätt sedan till ROT9.
+            while (k < 1) {
                 decrypted += (decrypt[j] + ROT7);
                 j++;
                 k++;
             }
-                if (decrypt[j] == '0') { // Om en nolla uppstår, hoppa över genom att öka j med ett.
+            // Om en nolla uppstår, hoppa över genom att öka j med ett.
+                if (decrypt[j] == '0') {
                     decrypted += (decrypt[j]);
                     j++;
                 }
             while (l < 1) {
-                decrypted += (decrypt[j] + ROT9); // Använd ROT9 en gång, fortsätt sedan till ROT7.
+                // Använd ROT9 en gång, fortsätt sedan till ROT7.
+                decrypted += (decrypt[j] + ROT9);
                 j++;
                 l++;
             }
             i++;
         }
     }
-    decrypted.erase(decrypted.size() - 1); // Tar bort en whitespace, hittar ingen snyggare lösning... än.
+    // Tar bort en whitespace, hittar ingen snyggare lösning... än.
+    decrypted.erase(decrypted.size() - 1);
     return decrypted;
 }
