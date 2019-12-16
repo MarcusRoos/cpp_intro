67a0694277301e75a60deb44f56cf38702fb659e
Marcus Roos <Marcusrs_roos@outlook.com>
2019-12-16 21:46:00 +0100

    Laboration 3 - Marcus Roos Senast uppdaterad: 16/12-2019
    Kommit en bit på vägen.

--- Laboration_3/src/Definitions.cpp
+++ Laboration_3/src/Definitions.cpp
@@ -18,8 +18,10 @@ long_type fibonacciRecursion(size_t nthNumber) {
 
 void mainMenu() {
     bool igen = true;
+    size_t nth=0;
+    vector <Stats> stats;
     do {
-        char input;
+        int input=0;
         cout << endl;
         cout << "FIBONACCI SEQUENCE MEASUREMENTS" << endl;
         cout << "===============================" << endl;
@@ -29,17 +31,22 @@ void mainMenu() {
         cout << "Your input: " << endl;
         cin >> input;
         switch (input) {
-            case '1':
-                size_t nth;
+            case 1:
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(),'\n');
                 nth = subMenu();
-                fibonacciTimer(nth);
+                stats=fibonacciTimer(nth);
+                printStats(stats);
                 break;
-            case '2':
+            case 2:
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(),'\n');
                 igen = false;
                 break;
         }
-        if (input != '1' && input != '2')
+        if (input != 1 && input != 2)
             cout << "fel input" << endl;
+        input = 0;
 
     } while (igen);
 }
@@ -54,6 +61,7 @@ size_t subMenu(){
             cin.clear();
             cin.ignore();
             cout << "not an int" << endl;
+            igen = true;
         }
         else
             igen = false;
@@ -62,14 +70,43 @@ size_t subMenu(){
     return nth;
 }
 
-vector<Stats> fibonacciTimer(size_t nthNumber){
-    int recursion=0, iteration=0;
-        recursion = fibonacciRecursion(nthNumber);
-        iteration = fibonacciIteration(nthNumber);
-        cout << "Iteration:" << iteration<< " Duration: " << endl;
-        cout << "Recursion:" << recursion<< " Duration: " << endl;
-        mainMenu();
+std::vector<Stats>fibonacciTimer(size_t nthNumber){
+    Stats recursion, iteration;
+    size_t nthTemp = nthNumber, nth=0;
+    string type;
+    auto timeStartR = std::chrono::high_resolution_clock::now();
+    int i=5;
+    do{
+        nth = fibonacciRecursion(nthTemp);
+        recursion.values.push_back(nth);
+        recursion.type="recursion";
+        if (i==5){
+            cout << setw(5) << left << "Recursions " << nthTemp << "'th value: " << setw(10) << right << nth << endl;
+            i=0;
+        }
+        nthTemp--, i++;
+    }while(nth!=0);
+    auto timeEndR = std::chrono::high_resolution_clock::now();
+    recursion.millisec = std::chrono::duration_cast<std::chrono::milliseconds>(timeEndR - timeStartR ).count();
+    nthTemp=nthNumber;
+    auto timeStartI = std::chrono::high_resolution_clock::now();
+    i=5;
+    do{
+        nth = fibonacciIteration(nthTemp);
+        recursion.values.push_back(nth);
+        recursion.type="recursion";
+        if (i==5){
+            cout << setw(5) << left << "Iterations " << nthTemp << "'th value: " << setw(10) << right << nth << endl;
+            i=0;
+        }
+        nthTemp--, i++;
+    }while(nth!=0);
+    auto timeEndI = std::chrono::high_resolution_clock::now();
+    iteration.millisec = std::chrono::duration_cast<std::chrono::milliseconds>(timeEndI - timeStartI ).count();
+    cout << "Recursion took: " << recursion.millisec << " Milliseconds" << endl;
+    cout << "Iteration took: " << iteration.millisec << " Milliseconds" << endl;
 
+    return {iteration, recursion};
 }
 
 void printStats(const std::vector<Stats>& stats){
